15b415a8008a1e3ac6c34692940a0a7388429321
Stephen Mathews <mathews.stephen@gmail.com>
2025-05-06 12:20:22 -0600

    working movement

--- main.py
+++ main.py
@@ -2,6 +2,133 @@ import curses, time, random
 
 max_x = 60
 max_y = 20
+dir_keys = "wasd"
+
+class Field:
+    _shrooms = []
+    _shroom_start_health = 3
+    
+    def __init__(self, size = (0,0), difficulty = 8):
+        self._size = size
+        self._difficulty = difficulty
+        self.reset()
+
+    def reset(self):
+        self._init_shrooms()
+
+    def _init_shrooms(self):
+        self._shrooms = []
+        max_x, max_y = self._size
+        for i in range(max_y - 1):
+            y = i + 1
+            x_check = []
+            for _ in range(self._difficulty):
+                x = random.randint(1, max_x)
+                if x in x_check:
+                    continue
+                self._shrooms.append([x, y, self._shroom_start_health])
+
+    def check_collision(self, pos, damage=False):
+        for i in range(len(self._shrooms)):
+            spos = tuple(self._shrooms[i][:2])
+            if spos == pos:
+                if damage:
+                    self._shrooms[i][2] -= 1
+                if self._shrooms[i][2] == 0:
+                    del self._shrooms[i]
+                return True
+        return False
+    
+    def check_boundary(self, pos):
+        x, y = pos
+        max_x, max_y = self._size
+        if x <= 0 or y <= 1:
+            return True
+        if x >= max_x or y >= max_y:
+            return True
+        return False
+
+    def draw(self, win):
+        for shroom in self._shrooms:
+            x, y, d = shroom
+            match d:
+                case 3:
+                    ch = "&"
+                case 2:
+                    ch = "#"
+                case 1:
+                    ch = "+"
+                case _:
+                    # Shouldn't happen
+                    # we may want to throw an exception
+                    ch = ""
+            win.addch(y, x, ord(ch))
+        
+    def get_shooter_start(self):
+        max_x, max_y = self._size
+        return (max_x//2, max_y)
+
+class Centipede:
+    def __init__(self, length, field):
+        self._length = length
+        self._field = field
+        self._body = [(1,1)]
+        self._direction = (1,0)
+
+    def draw(self, win):
+        for seg in self._body:
+            x, y = seg
+            win.addch(y, x, ord("0"))
+
+    def _reverse(self):
+        x, y = self._direction
+        self._direction = (x*-1, y)
+
+    def _avoid(self):
+        x, y = self._body[0]
+        self._reverse()
+        return (x, y+1)
+
+    def move(self):
+        x, y = self._body[0]
+        dx, dy = self._direction
+        n = (x + dx, y + dy)
+        if self._field.check_collision(n) or self._field.check_boundary(n):
+            n = self._avoid()
+        
+        self._body.insert(0, n)
+        if len(self._body) > self._length:
+            self._body = self._body[:self._length]
+
+class Shooter:
+    def __init__(self, field, centipede):
+        self._bullet = None
+        self._field = field
+        self._centipede = centipede
+        self._pos = field.get_shooter_start()
+
+    def draw(self, win):
+        x, y = self._pos
+        win.addch(y, x-1, ord("/"))
+        win.addch(y, x, ord("^"))
+        win.addch(y, x+1, ord('\\'))
+        if self._bullet != None:
+            bx, by = self._bullet
+            win.addch(by, bx, ord("|"))
+            if self._field.check_collision(self._bullet, True) or self._field.check_boundary(self._bullet):
+                self._bullet = None
+            else:
+                self._bullet = (bx, by-1)
+
+    def move(self, direction):
+        x, y = self._pos
+        dx, dy = direction
+        self._pos = (x+dx, y+dy)
+
+    def shoot(self):
+        x, y = self._pos
+        if self._bullet == None:
+            self._bullet = (x, y-1)
 
 def get_direction(key):
     match key:
@@ -13,24 +140,6 @@ def get_direction(key):
             return (0,1)
         case "d":
             return (1,0)
-        
-def new_food():
-    return (random.randint(1,max_x-1), random.randint(1,max_y-1))
-
-def draw_snake(win, snake):
-    for seg in snake:
-        x, y = seg
-        win.addch(y, x, ord("0"))
-
-def draw_food(win, food):
-    x, y = food
-    win.addch(y, x, ord("+"))
-
-def collision(head, snake):
-    for seg in snake:
-        if seg == head:
-            return True
-    return False
 
 def main(screen):
     curses.curs_set(0)
@@ -39,68 +148,39 @@ def main(screen):
     win = curses.newwin(max_y + 2, max_x + 2, 2, 5)
     win.box()
 
-    x, y = 0, 5
-    key = "d"
+    field = Field((max_x, max_y), difficulty=4)
+    centi = Centipede(10, field)
+    shoot = Shooter(field, centi)
 
-    snake_len = 5
-    snake = [(1,1)]
-    food = new_food()
+
+    count = 0
     while True:
-        x, y = snake[0]
-        dx, dy = get_direction(key)
-        x = x + dx
-        y = y + dy
-        if x == (max_x + 1):
-            x = 0
-        elif x == 0:
-            x = max_x
-
-        if y == (max_y + 1):
-            y = 0
-        elif y == 0:
-            y = max_y
-        
-        head = (x,y)
-
-        if collision(head, snake):
-            for i in range (0, 20):
-                screen.addstr(i, 0, "YOU DIED!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-                screen.refresh()
-                time.sleep(0.05)
-            break
-
-        if head == food:
-            snake_len += 5
-            food = new_food()
-
-        snake = [head] + snake
-        if len(snake) > snake_len:
-            snake = snake[:snake_len]
-
-        screen.addstr(1,0, "head: (%s,%s)" % head)
-        screen.refresh()
-        win.box()
-
-        win.clrtobot()
+        count += 1
         win.clear()
-        draw_snake(win, snake)
-        draw_food(win, food)
-        win.refresh()
         win.box()
 
+        field.draw(win)
+
+        if count % 8 == 0:
+            centi.move()
+        centi.draw(win)
 
         win.nodelay(True)
         try:
             key = win.getkey()
             if key != curses.ERR:
-                screen.addstr(0, 0, "You pressed: %s" % key)
-                screen.refresh()
-                win.box()
+                if key in dir_keys:
+                    dir = get_direction(key)
+                    shoot.move(dir)
+                if key == "q":
+                    shoot.shoot()
         except (curses.error):
             pass
 
+        shoot.draw(win)
+
         win.refresh()
-        time.sleep(0.1)
+        time.sleep(0.01)
     
 
     screen.refresh()
